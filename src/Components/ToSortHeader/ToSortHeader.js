075275b0a5020fccf731a6791e211bada82375be
Simon Situ <ssitu001@gmail.com>
2017-12-06 08:52:30 -0800

    add propTypes

--- src/Components/ToSortHeader/ToSortHeader.js
+++ src/Components/ToSortHeader/ToSortHeader.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class ToSortHeader extends Component {
-  handleClick = () => {
-    this.props.onClick(this.props.sortBy)
+
+const ToSortHeader = (props) => {
+  const handleClick = () => {
+    props.onClick(props.sortBy)
   }
 
-  render() {
-    return (
-      <th scope="col">{this.props.heading}<i onClick={this.handleClick} className="fa fa-fw fa-sort"></i></th>
-    )
-  }
+  return (
+    <th scope="col">{props.heading}<i onClick={handleClick} className="fa fa-fw fa-sort"></i></th>
+  )
 }
+
+ToSortHeader.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  sortBy: PropTypes.string,
+  header: PropTypes.string,
+};
 
 export default ToSortHeader;
